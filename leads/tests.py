fae641a18eca648768ec3f0721e03957c171a66a
leonard <leonadotienootieno@gmail.com>
2026-03-21 23:47:16 +0300

    Add basic landing and thank-you view tests

--- leads/tests.py
+++ leads/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class LandingViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_landing_get_renders(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Check Airtel 5G Availability')
+
+    def test_thankyou_view_renders(self):
+        response = self.client.get('/thank-you/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Thank You')
+
